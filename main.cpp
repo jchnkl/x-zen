69abfbbc14c6935c5c0ffb6e1a50451f416defa3
Jochen Keil <jochen.keil@gmail.com>
2013-09-28 13:10:27 +0200

    Add all available windows to client_manager

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@
 int main(int argc, char ** argv)
 {
   x::connection c("");
+  c.change_window_attributes(c.root(),
+                             XCB_CW_EVENT_MASK,
+                             { XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY });
   x::event::source s(c);
 
   zen::client_manager cm(c, s);
+
+  for (auto & window : c.query_tree(c.root())) {
+    cm.insert(window);
+  }
 
   s.run();
 
