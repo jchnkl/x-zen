d56d3bdd86a6d2d5ecbdc97cb68355b0f9c2f4a3
Jochen Keil <jochen.keil@gmail.com>
2013-09-28 11:59:26 +0200

    Implement empty configure notify handler

--- zen/client.hpp
+++ zen/client.hpp
@@ -1,15 +1,32 @@
 #ifndef X_CLIENT_HPP
 #define X_CLIENT_HPP
 
+#include <climits>
+
 #include "../x/window.hpp"
+#include "../x/interface.hpp"
 
 namespace x {
 
-class client : public window {
+class client : public window
+             , public interface::event::dispatcher
+             , public interface::event::sink<xcb_configure_notify_event_t>
+             {
   public:
     client(connection & c, xcb_window_t w)
       : window(c, w)
     {}
+
+    virtual priority_masks
+    masks(void)
+    {
+      return { { UINT_MAX, XCB_CONFIGURE_NOTIFY } };
+    }
+
+    virtual void
+    handle(xcb_configure_notify_event_t * e)
+    {
+    }
 
   private:
 }; // class client
