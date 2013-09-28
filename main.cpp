b1c86248a38b4ffd1dfc825c42b76c95979d0a2c
Jochen Keil <jochen.keil@gmail.com>
2013-09-28 12:24:03 +0200

    Put client manager on main stack

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "x/connection.hpp"
 #include "x/event.hpp"
+#include "zen/client_manager.hpp"
 
 int main(int argc, char ** argv)
 {
   x::connection c("");
   x::event::source s(c);
+
+  zen::client_manager cm(c, s);
 
   s.run();
 
