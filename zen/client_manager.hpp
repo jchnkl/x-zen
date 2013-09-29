53f8a22f9cec4fab2a7ca4474e8be9210db92a45
Jochen Keil <jochen.keil@gmail.com>
2013-09-29 21:47:15 +0200

    Prefix members with m_

--- zen/client_manager.hpp
+++ zen/client_manager.hpp
@@ -53,33 +53,39 @@ class client_manager
     void
     insert(xcb_window_t window)
     {
-      client_order.push_front(window);
-      clients[window] = client_ptr(new client(m_c, m_s, window));
+      m_client_order.push_front(window);
+      m_clients[window] = client_ptr(new client(m_c, m_s, window));
     }
 
     void
     remove(xcb_window_t window)
     {
-      client_order.erase(
-          std::find(client_order.begin(), client_order.end(), window));
-      clients.erase(window);
+      m_client_order.erase(
+          std::find(m_client_order.begin(), m_client_order.end(), window));
+      m_clients.erase(window);
+    }
+
+    const std::unordered_map<xcb_window_t, client_ptr> &
+    clients(void)
+    {
+      return m_clients;
     }
 
   private:
     connection & m_c;
     interface::event::source & m_s;
 
-    std::deque<xcb_window_t> client_order;
-    std::unordered_map<xcb_window_t, client_ptr> clients;
+    std::deque<xcb_window_t> m_client_order;
+    std::unordered_map<xcb_window_t, client_ptr> m_clients;
 }; // class client_manager
 
 std::ostream & operator<<(std::ostream & os, const client_manager & cm)
 {
   std::size_t i = 0;
-  for (i = 0; i < cm.client_order.size(); ++i) {
-    os << *cm.clients.at(cm.client_order[i]);
+  for (i = 0; i < cm.m_client_order.size(); ++i) {
+    os << *cm.m_clients.at(cm.m_client_order[i]);
 
-    if (i < cm.client_order.size() - 1) {
+    if (i < cm.m_client_order.size() - 1) {
       os << ", ";
     }
   }
