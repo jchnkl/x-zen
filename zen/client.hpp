e82649d78bdd318bc251200380839bb1746a5dc8
Jochen Keil <jochen.keil@gmail.com>
2013-09-29 21:53:08 +0200

    Enable moving the window on mouse click
    Preliminary: not configurable, etc.

--- zen/client.hpp
+++ zen/client.hpp
@@ -19,6 +19,8 @@ typedef std::shared_ptr<client> client_ptr;
 class client : public window
              , public interface::event::dispatcher
              , public interface::event::sink<xcb_map_request_event_t>
+             , public interface::event::sink<xcb_button_press_event_t>
+             , public interface::event::sink<xcb_motion_notify_event_t>
              , public interface::event::sink<xcb_configure_request_event_t>
              {
   public:
@@ -28,17 +30,37 @@ class client : public window
       : window(c, w), m_s(s)
     {
       s.insert(this);
+      auto reply = get_attributes();
+      if (! reply->override_redirect) {
+        change_attributes(XCB_CW_EVENT_MASK, { XCB_EVENT_MASK_KEY_PRESS
+                                             | XCB_EVENT_MASK_KEY_RELEASE
+                                             });
+
+        grab_button(false,
+                    XCB_EVENT_MASK_BUTTON_PRESS
+                    | XCB_EVENT_MASK_BUTTON_RELEASE
+                    | XCB_EVENT_MASK_BUTTON_MOTION,
+                    XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE,
+                    XCB_BUTTON_INDEX_1, XCB_MOD_MASK_ANY);
+
+      }
     }
 
     ~client(void)
     {
       m_s.remove(this);
+
+      ungrab_key(XCB_GRAB_ANY, XCB_MOD_MASK_ANY);
+      ungrab_button(XCB_BUTTON_INDEX_ANY, XCB_MOD_MASK_ANY);
     }
 
     priority_masks
     masks(void)
     {
       return { { UINT_MAX, XCB_CONFIGURE_REQUEST }
+             , { UINT_MAX, XCB_BUTTON_PRESS }
+             , { UINT_MAX, XCB_BUTTON_RELEASE }
+             , { UINT_MAX, XCB_MOTION_NOTIFY }
              , { UINT_MAX, XCB_MAP_REQUEST }
              };
     }
@@ -69,12 +91,31 @@ class client : public window
     }
 
     void
-    handle(xcb_configure_notify_event_t * e)
+    handle(xcb_button_press_event_t * e)
     {
+      if (e->event != m_window) return;
+
+      if ((e->response_type & ~0x80) == XCB_BUTTON_PRESS) {
+        m_pointer_offset_x = e->event_x;
+        m_pointer_offset_y = e->event_y;
+      }
+    }
+
+    void
+    handle(xcb_motion_notify_event_t * e)
+    {
+      if (e->event != m_window) return;
+
+      configure(XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
+                { static_cast<uint32_t>(e->root_x - m_pointer_offset_x),
+                  static_cast<uint32_t>(e->root_y - m_pointer_offset_y) });
     }
 
   private:
     interface::event::source & m_s;
+
+    unsigned int m_pointer_offset_x;
+    unsigned int m_pointer_offset_y;
 }; // class client
 
 std::ostream & operator<<(std::ostream & os, const client & c)
