29175fc1ff3c12c8d48d4f08d8dbc82d1e6b0901
Jochen Keil <jochen.keil@gmail.com>
2013-09-29 21:46:34 +0200

    Remove window from method names

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "x/connection.hpp"
 #include "x/event.hpp"
 #include "zen/client_manager.hpp"
+#include "zen/input.hpp"
 
 int main(int argc, char ** argv)
 {
   x::connection c("");
-  c.change_window_attributes(c.root(),
-                             XCB_CW_EVENT_MASK,
-                             { XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
-                             | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
-                             });
+  c.change_attributes(c.root(), XCB_CW_EVENT_MASK,
+                      { XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+                      | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+                      });
 
   x::event::source s(c);
 
