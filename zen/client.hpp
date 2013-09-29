6433fd16a6be6bda56bab8f70bc203fdae631e82
Jochen Keil <jochen.keil@gmail.com>
2013-09-29 21:49:17 +0200

    Add event handlers for {configure,map} request events

--- zen/client.hpp
+++ zen/client.hpp
@@ -18,7 +18,8 @@ typedef std::shared_ptr<client> client_ptr;
 
 class client : public window
              , public interface::event::dispatcher
-             , public interface::event::sink<xcb_configure_notify_event_t>
+             , public interface::event::sink<xcb_map_request_event_t>
+             , public interface::event::sink<xcb_configure_request_event_t>
              {
   public:
     friend std::ostream & operator<<(std::ostream &, const client &);
@@ -37,7 +38,34 @@ class client : public window
     priority_masks
     masks(void)
     {
-      return { { UINT_MAX, XCB_CONFIGURE_NOTIFY } };
+      return { { UINT_MAX, XCB_CONFIGURE_REQUEST }
+             , { UINT_MAX, XCB_MAP_REQUEST }
+             };
+    }
+
+    void
+    handle(xcb_map_request_event_t * e)
+    {
+      if (e->window != m_window) return;
+      map();
+    }
+
+    void
+    handle(xcb_configure_request_event_t * e)
+    {
+      if (e->window != m_window) return;
+
+      std::vector<uint32_t> values;
+
+      if (e->value_mask & XCB_CONFIG_WINDOW_X)            values.push_back(e->x);
+      if (e->value_mask & XCB_CONFIG_WINDOW_Y)            values.push_back(e->y);
+      if (e->value_mask & XCB_CONFIG_WINDOW_WIDTH)        values.push_back(e->width);
+      if (e->value_mask & XCB_CONFIG_WINDOW_HEIGHT)       values.push_back(e->height);
+      if (e->value_mask & XCB_CONFIG_WINDOW_BORDER_WIDTH) values.push_back(e->border_width);
+      if (e->value_mask & XCB_CONFIG_WINDOW_SIBLING)      values.push_back(e->sibling);
+      if (e->value_mask & XCB_CONFIG_WINDOW_STACK_MODE)   values.push_back(e->stack_mode);
+
+      configure(e->value_mask, values);
     }
 
     void
