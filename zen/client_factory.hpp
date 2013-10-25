e1bff13d84f1a1a845e82027ae89ae39f0587473
Jochen Keil <jochen.keil@gmail.com>
2013-10-25 21:42:23 +0200

    Construct new clients in concrete factory
    Now moveable & resizable!

--- zen/client_factory.hpp
+++ zen/client_factory.hpp
@@ -1,8 +1,10 @@
 #ifndef ZEN_CLIENT_FACTORY_HPP
 #define ZEN_CLIENT_FACTORY_HPP
 
+#include "../x/cursor.hpp"
 #include "../x/interface.hpp"
 #include "../zen/client.hpp"
+#include "../zen/pointer.hpp"
 #include "../zen/interface.hpp"
 
 namespace zen {
@@ -14,30 +16,28 @@ namespace xevent = x::interface::event;
 
 class factory : public interface::client::factory {
   public:
-    factory(x::connection & c, xevent::source & s)
-      : m_c(c), m_s(s)
+    factory(x::connection & c, xevent::source & s, x::cursor & cursor)
+      : m_c(c), m_s(s), m_cursor(cursor)
     {}
 
     interface::client::ptr make(const xcb_window_t & window)
     {
-      auto new_client =
-        std::shared_ptr<client>(
-            new client(m_c, m_s,
-                       window));
+      auto c = std::shared_ptr<interface::client>(
+          new client(m_c, m_s, window));
 
-      return new_client;
-    }
+      c = std::shared_ptr<interface::client>(
+          new pointer::move(c, m_c, m_s, m_cursor));
 
+      c = std::shared_ptr<interface::client>(
+          new pointer::resize(c, m_c, m_s, m_cursor));
 
-
-
-
-
+      return c;
     }
 
   private:
     x::connection & m_c;
     xevent::source & m_s;
+    x::cursor & m_cursor;
 
 }; // class factory
 
