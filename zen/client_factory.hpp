9e2c9fa1acb2b38a8aebab71dacb0e61d6fbe7a9
Jochen Keil <jochen.keil@gmail.com>
2013-10-28 21:30:55 +0100

    Adapt client factory to new interface

--- zen/client_factory.hpp
+++ zen/client_factory.hpp
@@ -4,45 +4,35 @@
 #include "../x/cursor.hpp"
 #include "../x/interface.hpp"
 #include "../zen/client.hpp"
-#include "../zen/pointer.hpp"
 #include "../zen/interface.hpp"
 
 namespace zen {
 
-namespace client {
-
-using zen::interface::event;
 namespace xevent = x::interface::event;
 
-class factory : public interface::client::factory {
+namespace client {
+
+class factory : public zen::interface::client::factory {
   public:
-    factory(x::connection & c, xevent::source & s, x::cursor & cursor)
-      : m_c(c), m_s(s), m_cursor(cursor)
+    factory(x::connection & c, xevent::source & s)
+      : m_c(c), m_s(s)
     {}
 
-    interface::client::ptr make(const xcb_window_t & window)
+    client::ptr
+    make(const xcb_window_t & window, const client::ptr & c)
     {
-      auto c = std::shared_ptr<interface::client>(
+      return std::shared_ptr<interface::client>(
           new client(m_c, m_s, window));
-
-      c = std::shared_ptr<interface::client>(
-          new pointer::move(c, m_c, m_s, m_cursor));
-
-      c = std::shared_ptr<interface::client>(
-          new pointer::resize(c, m_c, m_s, m_cursor));
-
-      return c;
     }
 
   private:
     x::connection & m_c;
     xevent::source & m_s;
-    x::cursor & m_cursor;
 
 }; // class factory
 
-}; // namespace zen
-
 }; // namespace client
+
+}; // namespace zen
 
 #endif // ZEN_CLIENT_FACTORY_HPP
