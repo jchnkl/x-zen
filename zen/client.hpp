1969ae941c6ad786d2a8b5b8bac8b5026c347bf9
Jochen Keil <jochen.keil@gmail.com>
2013-10-28 21:33:28 +0100

    Set event mask for button press/release events on clients

--- zen/client.hpp
+++ zen/client.hpp
@@ -39,6 +39,8 @@ class client : public interface::client
                           , XCB_EVENT_MASK_ENTER_WINDOW
                           | XCB_EVENT_MASK_LEAVE_WINDOW
                           | XCB_EVENT_MASK_FOCUS_CHANGE
+                          | XCB_EVENT_MASK_BUTTON_PRESS
+                          | XCB_EVENT_MASK_BUTTON_RELEASE
                           });
 
         window::configure(XCB_CONFIG_WINDOW_BORDER_WIDTH, { 1 });
