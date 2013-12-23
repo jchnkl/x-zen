97d94b5b910082135952d009e0807349164c35bc
Jochen Keil <jochen.keil@gmail.com>
2013-12-23 11:44:23 +0100

    Remove useless wrapper files

--- zen/event.cpp
+++
@@ -1,6 +0,0 @@
-#ifndef ZEN_EVENT_CPP
-#define ZEN_EVENT_CPP
-
-#include "event.hpp"
-
-#endif // ZEN_EVENT_CPP
--- zen/pointer.cpp
+++
@@ -1,6 +0,0 @@
-#ifndef ZEN_POINTER_CPP
-#define ZEN_POINTER_CPP
-
-#include "pointer.hpp"
-
-#endif // ZEN_POINTER_CPP
