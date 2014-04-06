440a3c40e17185ab5bad96b32b48a785dd599d18
Jochen Keil <jochen.keil@gmail.com>
2014-04-06 12:16:06 +0200

    EWMH root window properety stubs

--- test/ewmh.hpp
+++ test/ewmh.hpp
@@ -25,6 +25,163 @@ class ewmh
     {
     }
 
+    void
+    get_net_supported(void)
+    {
+    }
+
+    void
+    set_net_supported(void)
+    {
+    }
+
+    void
+    get_net_client_list(void)
+    {
+    }
+
+    void
+    set_net_client_list(void)
+    {
+    }
+
+    void
+    get_net_number_of_desktops(void)
+    {
+    }
+
+    void
+    set_net_number_of_desktops(void)
+    {
+    }
+
+    void
+    get_net_desktop_geometry(void)
+    {
+    }
+
+    void
+    set_net_desktop_geometry(void)
+    {
+    }
+
+    void
+    get_net_desktop_viewport(void)
+    {
+    }
+
+    void
+    set_net_desktop_viewport(void)
+    {
+    }
+
+    void
+    get_net_current_desktop(void)
+    {
+    }
+
+    void
+    set_net_current_desktop(void)
+    {
+    }
+
+    void
+    get_net_desktop_names(void)
+    {
+    }
+
+    void
+    set_net_desktop_names(void)
+    {
+    }
+
+    void
+    get_net_active_window(void)
+    {
+    }
+
+    void
+    set_net_active_window(void)
+    {
+    }
+
+    void
+    get_net_workarea(void)
+    {
+    }
+
+    void
+    set_net_workarea(void)
+    {
+    }
+
+    void
+    get_net_supporting_wm_check(void)
+    {
+    }
+
+    void
+    set_net_supporting_wm_check(void)
+    {
+    }
+
+    void
+    get_net_virtual_roots(void)
+    {
+    }
+
+    void
+    set_net_virtual_roots(void)
+    {
+    }
+
+    void
+    get_net_desktop_layout(void)
+    {
+    }
+
+    void
+    set_net_desktop_layout(void)
+    {
+    }
+
+    void
+    get_net_showing_desktop(void)
+    {
+    }
+
+    void
+    set_net_showing_desktop(void)
+    {
+    }
+
+    // other
+
+    void
+    handle_net_close_window(void)
+    {
+    }
+
+    void
+    handle_net_moveresize_window(void)
+    {
+    }
+
+    void
+    handle_net_wm_moveresize(void)
+    {
+    }
+
+    void
+    handle_net_restack_window(void)
+    {
+    }
+
+    void
+    handle_net_request_frame_extents(void)
+    {
+    }
+
   private:
     // x::connection & m_c;
     // x::registry & m_registry;
